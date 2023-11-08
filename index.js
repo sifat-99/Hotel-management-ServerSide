a3b57c655321cb1391befa080c9446943f03b4ab
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2023-11-08 14:01:06 +0600

    varify token middleware added

--- index.js
+++ index.js
@@ -41,7 +41,16 @@ const logger = (req, res, next) => {
   next();
 }
 
-
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if(!token) return res.sendStatus(403);
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if(err) return res.sendStatus(403).send({message: 'Unauthorized'});
+      req.user = decoded;
+      next();
+  }
+  )
+}
 
 
 async function run() {
